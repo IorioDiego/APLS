89eb793b8dcf2d7aac766fc28a266f7e2231dd73
Diego <dieguito98ig@gmail.com>
2021-06-21 00:46:50 -0300

    Pueden jugar pero con cuidado

--- apl3/Ejercicio5/Ejercicio5Cliente.cpp
+++ apl3/Ejercicio5/Ejercicio5Cliente.cpp
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -16,31 +17,69 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <semaphore.h>
 
 using namespace std;
 void mostrarPalabra(char * );
 void handlerSigInt(int sig );
-
+void dibujarHangman(int);
+ string  validarIngreso();
+void hayJugadorActivo();
+void help();
 
  int socketComunicacion ;
+sem_t *semBlockCliente;
 
 
 int main(int argc, char *argv[]){
 
+  
+    if (argc < 2 || argc > 3) {
+        cout << "La cantidad de parametros es incorrecta" << endl;
+        return 1;
+    }
+    else if (argc == 2 && (!strcmp(argv[1],"--help") || !strcmp(argv[1], "-h"))) {
+        help();
+        return 0;
+    }else if(argc == 2 && (strcmp(argv[1],"--help") || strcmp(argv[1], "-h"))){
+        cout << "Parametros incorrectos" << endl;
+        return 1;
+    } else if (argc == 3 && (atoi(argv[2])<=1023 ) ) {
+        cout << "El numero de puerto debe estar por encima de 1023 " << endl;
+        return 0;
+    }
 
+    
     struct sigaction action;
      action.sa_handler = handlerSigInt;
      sigaction(SIGINT,&action,NULL);
 
     //Validacion//
     
+    //fin validacion
+ semBlockCliente = sem_open("bloqueoCliente", O_CREAT, 0600, 1);
+
+//     int valorSem;
+       
+//  //
+//     sem_getvalue(semBlockCliente, &valorSem);
+//     if (!valorSem)
+//     {
+//         hayJugadorActivo();
+//         return 0;
+//     }
+
+//     sem_wait(semBlockCliente); //-->un solo cliente
+
 
     //configuracion de socket //
     struct sockaddr_in socketConfig;
     memset(&socketConfig, '0', sizeof(socketConfig));
 
     socketConfig.sin_family = AF_INET;
-    socketConfig.sin_port = htons(5000);
+    socketConfig.sin_port = htons((atoi(argv[2])));
     inet_pton(AF_INET, argv[1], &socketConfig.sin_addr); 
 
     socketComunicacion = socket(AF_INET, SOCK_STREAM, 0);
@@ -53,41 +92,124 @@ int main(int argc, char *argv[]){
             return EXIT_FAILURE;
         }
          string ingreso;
-        char buffer[2000];
+        char sBuffer[2000];
+        char rBuffer[2000];
         int bytesRecibidos = 0;
 
    
-
-    do
-{
-     bytesRecibidos = read (socketComunicacion,buffer,sizeof(buffer)-1 );
-
-    buffer[bytesRecibidos] = 0;
-     // printf("%s\n",buffer);
     
-    //escribo palabra o letra
-   mostrarPalabra(buffer);
-    cout<< "ingrese letra o palabra"<<endl;
-     getline(cin, ingreso);
-   write(socketComunicacion,ingreso.c_str(),strlen(ingreso.c_str()));
+        do
+        {//aca
+             //system("clear"); 
+            
+            ////recibir cant intentos////
+            bytesRecibidos = 0;
+            bytesRecibidos = read (socketComunicacion,rBuffer,sizeof(rBuffer)-1 ); 
+            rBuffer[bytesRecibidos] = 0;
+            cout << "Intentos restantes : "<< rBuffer<< endl;
+            char c = rBuffer[0];
+  
+            //// fin recibir cant intentos////
+            dibujarHangman(atoi(&c));
+            
+             //envio confirmacion para que pueda seguir el server
+            snprintf(sBuffer,sizeof(sBuffer),"%s\r\n", "OK");
+             write(socketComunicacion,sBuffer,strlen(sBuffer));
+            //fin envio confirmacion para que pueda seguir el server
+            
 
-    //recivo resultado
-    bytesRecibidos = read (socketComunicacion,buffer,sizeof(buffer)-1 );
-    buffer[bytesRecibidos] = 0;
+            ////recibir palabra a adivinar////
+            bytesRecibidos = 0;
+            bytesRecibidos = read (socketComunicacion,rBuffer,sizeof(rBuffer)-1 ); 
+            rBuffer[bytesRecibidos] = 0;
+            mostrarPalabra(rBuffer);
+            //// fin recibir palabra a adivinar////
 
-    cout << buffer<< endl;
 
-    //lee si fue fin o no
-     bytesRecibidos = read (socketComunicacion,buffer,sizeof(buffer)-1 );
-    buffer[bytesRecibidos] = 0;
+             //// ingreso letra o palabra que pienso q es////
+        
+            cout<< "ingrese letra o palabra"<<endl;
+            getline(cin, ingreso);
 
-    cout<<buffer<< endl;
+        //ingreso =   validarIngreso();
+            
+            snprintf(sBuffer,sizeof(sBuffer),"%s", ingreso.c_str());
+            write(socketComunicacion,sBuffer,strlen(sBuffer));
+             ////fin  ingreso letra o palabra que pienso q es////
 
-}while ( strcmp(buffer,"FIN\n" ) != 0  );
- 
-cout << "terminar" << endl;
-close(socketComunicacion);
-return EXIT_SUCCESS;
+            //recivo resultado(palbara o letra correcta) //
+            bytesRecibidos = 0;
+            bytesRecibidos = read (socketComunicacion,rBuffer,sizeof(rBuffer)-1 );
+            rBuffer[bytesRecibidos] = 0;
+            printf("resultado %s\n", rBuffer);
+            ////fin recivo resultado ///
+
+            //envio confirmacion para que pueda seguir el server
+            snprintf(sBuffer,sizeof(sBuffer),"%s\r\n", "OK");
+            write(socketComunicacion,sBuffer,strlen(sBuffer));
+            //fin envio confirmacion para que pueda seguir el server
+
+
+            //lee si fue fin o no (si perdi o si adivine la palabra)    
+            bytesRecibidos = 0;
+            bytesRecibidos = read (socketComunicacion,rBuffer,sizeof(rBuffer)-1 );
+            rBuffer[bytesRecibidos] = 0;
+            printf("resultado :%s\n", rBuffer);
+          
+            // fin lee si fue fin o no (si perdi o si adivine la palabra)    
+
+            //envio confirmacion para que pueda seguir el server
+            snprintf(sBuffer,sizeof(sBuffer),"%s\r\n", "OK");
+             write(socketComunicacion,sBuffer,strlen(sBuffer));
+            //fin envio confirmacion para que pueda seguir el server
+
+         
+
+        }while ( strcmp(rBuffer,"FIN\r\n" ) != 0  );
+              system("clear"); 
+        //Read
+
+         //lee si gane o perdi    
+         bytesRecibidos = 0;
+         bytesRecibidos = read (socketComunicacion,rBuffer,sizeof(rBuffer)-1 );
+         rBuffer[bytesRecibidos] = 0;
+         printf("resultado :%s\n", rBuffer);
+        //fin lee si gane o perdi  
+
+        if(strcmp(rBuffer,"PERDISTE\r\n")==0){
+                dibujarHangman(0);
+        }else
+        {
+            dibujarHangman(7);
+        }
+
+        //write
+        //envio confirmacion para que pueda seguir el server
+        snprintf(sBuffer,sizeof(sBuffer),"%s\r\n", "OK");
+         write(socketComunicacion,sBuffer,strlen(sBuffer));
+        //fin envio confirmacion para que pueda seguir el server
+
+
+        ////recibir palabra a adivinar////
+            bytesRecibidos = 0;
+            bytesRecibidos = read (socketComunicacion,rBuffer,sizeof(rBuffer)-1 ); 
+            rBuffer[bytesRecibidos] = 0;
+            cout << "La palabra era : " << endl;
+            mostrarPalabra(rBuffer);
+            //// fin recibir palabra a adivinar////
+    
+
+        //write
+        //envio confirmacion para que pueda seguir el server
+        snprintf(sBuffer,sizeof(sBuffer),"%s\r\n", "OK");
+         write(socketComunicacion,sBuffer,strlen(sBuffer));
+        //fin envio confirmacion para que pueda seguir el server
+
+
+        cout << "terminar" << endl;
+        sem_post(semBlockCliente);
+        close(socketComunicacion);
+        return EXIT_SUCCESS;
 
 
 
@@ -106,9 +228,176 @@ void mostrarPalabra(char * p){
 
 void handlerSigInt(int sig ){
 
-   
+     sem_close(semBlockCliente);
+    sem_unlink("bloqueoCliente");
      close(socketComunicacion);
      exit(sig);
 
 
+}
+
+string  validarIngreso()
+{
+    int cont = 0;
+    string ingreso;
+    char ca = '\n';
+    getline(cin, ingreso);
+    if(ingreso.size() > 1){
+        const char *ini = ingreso.c_str(); 
+        while(*ini != '\0'){
+            if(*ini=='\n'){
+                cont ++;
+   
+            }
+            ini++;
+        }
+    cout << "cantidad"<< cont <<endl;
+    }
+   // cout <<"HOLAAAAAAAa" << endl;
+    return ingreso;
+ 
+}
+
+void hayJugadorActivo()
+{
+    cout << "Error,otro jugador se encuentra en la sala" << endl;
+    cout << "Intente mas tarde" << endl;
+}
+
+
+void dibujarHangman(int n) {
+
+
+
+        switch(n) 
+        {
+                    case 0:
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+     "<<endl 
+                <<"   |    |     "<<endl 
+                <<"   |    O     "<<endl 
+                <<"   |   /|\\   "<<endl 
+                <<"   |   / \\   "<<endl 
+                <<"   | Moriste "<<endl 
+                <<"  ============"<<endl<<endl; 
+                    break;
+                    } 
+
+                    case 1:
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |    O  "<<endl 
+                <<"   |   /|\\ "<<endl 
+                <<"   |     \\ "<<endl 
+                <<"   |       "<<endl 
+                <<"  ============"<<endl<<endl; 
+                    break;
+                    }
+                
+                
+                    case 2: 
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |    O  "<<endl 
+                <<"   |   /|\\ "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"  ============"<<endl<<endl;         
+                        break;
+                    }
+                
+                    case 3: 
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |    O  "<<endl 
+                <<"   |   /|  "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"  ============"<<endl<<endl; 
+                    break;
+                    }
+
+                    
+                    case 4:
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |    O  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"  ============="<<endl<<endl; 
+                    break;
+                    }
+                    
+
+                    case 5:
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |    O  "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"  ============"<<endl<<endl; 
+                        break;
+                    }
+                    
+
+                    case 6:
+                    {
+                        cout<<endl<<endl 
+                <<"   +----+  "<<endl 
+                <<"   |    |  "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"   |       "<<endl 
+                <<"  ============"<<endl<<endl; 
+                break;
+                    }
+
+                case 7: {
+                     cout<<endl<<endl 
+                <<"   +----+     "<<endl 
+                <<"   |    |     "<<endl 
+                <<"   |          \\ O     "<<endl 
+                <<"   |            | \\   "<<endl 
+                <<"   |           / \\   "<<endl 
+                <<"   | Sobreviviste "<<endl 
+                <<"  ============"<<endl<<endl; 
+                }
+
+
+
+                }
+
+
+
+               
+}
+
+
+void help()
+{   
+    cout << "-------------------------------------------------------" << endl;
+    cout << "Descripción : El Script ejecuta el cliente que jugara al HangMan " << endl;
+    cout << "conectandose al mismo puerto e IP que el servidor.Podra hacerlo" << endl;
+    cout << "solo si hay un servidor conectado y si no hay otro jugador conectado" << endl;
+    cout << "\t- Ayuda del Script   Ejercicio5Cliente..." << endl;
+    cout << "\t- Nombre Script:     Ejercicio5Cliente 127.0.0.1 5000 "  << endl;
+    cout << "\t- Ejemplo de uso:   ./Ejercicio5Cliente" << endl;
+    cout << "\t- Ejemplo de ayuda:    ./Ejercicio4Servidor -h"<< endl;
+    cout << "\t- Ejemplo de ayuda:    ./Ejercicio4Servidor --help" << endl;
+    cout << "\t- Fin de la ayuda..." << endl;
+    cout << "-------------------------------------------------------" << endl;
+ 
 }
--- apl3/Ejercicio5/Ejercicio5Servidor.cpp
+++ apl3/Ejercicio5/Ejercicio5Servidor.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -15,17 +16,24 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+
 
 #define LETRA 1
 #define PALABRA 2
+#define INTENTOS 6
 
 using namespace std;
 void handlerSigInt(int sig );
-int compararConPalabraOculta(string,char * sendBuff ,int sizeBf,
-const char* pElegida,char* palabraOculta);
+list<char>  compararConPalabraOculta(string,char * sendBuff ,int sizeBf,
+const char* pElegida,char* palabraOculta,list<char>);
+void help();
+ void recorrerLisra(list<char> lista);
+string listToString(list<char> lista);
+ sem_t *semBlockServer;
  
- 
- 
+ int finJuego=0;
  int socketComunicacion;
 
 
@@ -33,20 +41,50 @@ string elegirPalabra();
 
 int main(int argc, char *argv[]){
 
+    if (argc != 2 ) {
+        cout << "La cantidad de parametros es incorrecta" << endl;
+        return 1;
+    }
+    else if ( (!strcmp(argv[1],"--help") || !strcmp(argv[1], "-h"))) {
+        help();
+        return 0;
+    }
+    else if ( (atoi(argv[1])<=1023 ) ){
+        cout << "El numero de puerto debe estar por encima de 1023 " << endl;
+        return 0;
+    }
+
+
+
     struct sigaction action;
      action.sa_handler = handlerSigInt;
      sigaction(SIGINT,&action,NULL);
-
-
+      
     //Validacion//
-    
+    ///fin validacion//    
+
+    semBlockServer = sem_open("bloqueoServer", O_CREAT, 0600, 1);
+
+    // int valorSem;
+    //  sem_getvalue(semBlockServer, &valorSem);
+
+    //  if (!valorSem)
+    //  {
+    //      cout << "Error,El servidor ya ha sido iniciado" << endl;
+    //     return 0;
+    //  }
+    //  sem_wait(semBlockServer);
+
+
+
+
 
     //configuracion de server //
     struct sockaddr_in serverConfig;
     memset(&serverConfig, '0' , sizeof(serverConfig));
     serverConfig.sin_family = AF_INET;//127.0.0.1
     serverConfig.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverConfig.sin_port = htons(5000);
+    serverConfig.sin_port = htons(atoi(argv[1]));
 
     int socketEscucha = socket(AF_INET,SOCK_STREAM,0);
     bind (socketEscucha,(struct sockaddr*) &serverConfig,sizeof(serverConfig));
@@ -54,8 +92,12 @@ int main(int argc, char *argv[]){
     listen(socketEscucha,1);
   
     while (true)
-    { 
+    {   
+            list<char>regLetras;
+            finJuego = 0;
+            cout << "Esperando Jugador....." << endl;
             socketComunicacion = accept(socketEscucha, (struct sockaddr *)NULL, NULL);
+            cout << "Jugador conectado" << endl;
             char sendBuff[2000];
             char readBuff[2000];
             int bytesRecibidos = 0;
@@ -79,45 +121,115 @@ int main(int argc, char *argv[]){
             }
 
         
-        do{
-                    //ENVIO PALABRA OCULTA
+        do{//aca
+               
+                 //ENVIO intentos
+                snprintf(sendBuff,sizeof(sendBuff),"%d\n", (INTENTOS -finJuego));
+                write(socketComunicacion,sendBuff,strlen(sendBuff) );
+                //FIN ENVIO intentos
+               
+             
 
-                snprintf(sendBuff,sizeof(sendBuff),"%s\n", palabraOculta);
-                    write(socketComunicacion,sendBuff,strlen(sendBuff) );
-
-            //RECIVO LETRA O PALABRA
+                    //LEO confirmacion para seguir
+                bytesRecibidos = 0;
                 bytesRecibidos = read (socketComunicacion,readBuff,sizeof(readBuff)-1 );
-                    
-                    readBuff[bytesRecibidos] = 0;
-                    
-                ///recibi letra o palabra // 
-                    mensaje = string(readBuff);
-                    cout<< mensaje << endl;
+                readBuff[bytesRecibidos] = 0;
+                //fin LEO confirmacion para seguir
 
+                   //ENVIO PALABRA OCULTA
+                snprintf(sendBuff,sizeof(sendBuff),"%s\n", palabraOculta);
+                write(socketComunicacion,sendBuff,strlen(sendBuff) );
+                //FIN ENVIO PALABRA OCULTA
+
+                //RECIVO LETRA O PALABRA
+                bytesRecibidos = 0;
+                bytesRecibidos = read (socketComunicacion,readBuff,sizeof(readBuff)-1 );
+                readBuff[bytesRecibidos] = 0;
+                mensaje = string(readBuff);
+                cout<< "letra recivida : " << mensaje << endl;
+                //FIN RECIVO LETRA O PALABRA
             
                 //comparo la letra o palabra //
-                 compararConPalabraOculta(mensaje,sendBuff,sizeof(sendBuff),palabraElegida.c_str(),palabraOculta);
-                
-                            //le manda el resultado
+              regLetras= compararConPalabraOculta(mensaje,sendBuff,sizeof(sendBuff),palabraElegida.c_str(),palabraOculta,regLetras);
+                            //le manda el resultado8si acerto o no)
                 write(socketComunicacion,sendBuff,strlen(sendBuff));
-
-
+                recorrerLisra(regLetras);
+                //FIN comparo la letra o palabra //
+                
+                ///Termino la partida si adivine lapalabra
+                 if(strcmp(palabraOculta,palabraElegida.c_str())==0){
+                   finJuego = INTENTOS;
+               }
+                ///termino la partida si adivine la palabra
+                
+                //LEO confirmacion para seguir
+                bytesRecibidos = 0;
+                bytesRecibidos = read (socketComunicacion,readBuff,sizeof(readBuff)-1 );
+                readBuff[bytesRecibidos] = 0;
+                //fin LEO confirmacion para seguir
+                
                 //envia fin o  no fin
                 if(strcmp(palabraOculta , palabraElegida.c_str()) != 0 ){
-
-                snprintf(sendBuff,sizeof(sendBuff),"%s\n", "NOFIN");
-                
+                    snprintf(sendBuff,sizeof(sendBuff),"%s\r\n", "NOFIN");
                 }else
                 {
-
-                snprintf(sendBuff,sizeof(sendBuff),"%s\n", "FIN");
-                
+                    snprintf(sendBuff,sizeof(sendBuff),"%s\r\n", "FIN");
                 }
+                
+                if(finJuego == INTENTOS){
+                    snprintf(sendBuff,sizeof(sendBuff),"%s\r\n", "FIN");
+                }
+
                 write(socketComunicacion,sendBuff,strlen(sendBuff));
+                //FIN envia fin o  no fin
 
-        } while( strcmp(palabraOculta , palabraElegida.c_str()) != 0 );
+                //LEO confirmacion para seguir
+                bytesRecibidos = 0;
+                bytesRecibidos = read (socketComunicacion,readBuff,sizeof(readBuff)-1 );    
+                readBuff[bytesRecibidos] = 0;
+           
+                //LEO confirmacion para seguir
 
 
+        // } while( (strcmp(palabraOculta , palabraElegida.c_str()) != 0)  && (finJuego != INTENTOS) );
+            } while( finJuego != INTENTOS  );
+
+        //write
+
+        if(strcmp(palabraOculta , palabraElegida.c_str()) == 0 )
+        {
+            snprintf(sendBuff,sizeof(sendBuff),"%s\r\n", "GANASTE");
+        } 
+        else
+        {
+            snprintf(sendBuff,sizeof(sendBuff),"%s\r\n", "PERDISTE");
+        }
+         write(socketComunicacion,sendBuff,strlen(sendBuff));
+
+        //Read
+         //LEO confirmacion para seguir
+        bytesRecibidos = 0;
+        bytesRecibidos = read (socketComunicacion,readBuff,sizeof(readBuff)-1 );    
+        readBuff[bytesRecibidos] = 0;
+    
+        //LEO confirmacion para seguir
+
+
+               //ENVIO PALABRA OCULTA
+                snprintf(sendBuff,sizeof(sendBuff),"%s\n", palabraElegida.c_str());
+                write(socketComunicacion,sendBuff,strlen(sendBuff) );
+                //FIN ENVIO PALABRA OCULTA
+
+
+            //Read
+         //LEO confirmacion para seguir
+        bytesRecibidos = 0;
+        bytesRecibidos = read (socketComunicacion,readBuff,sizeof(readBuff)-1 );    
+        readBuff[bytesRecibidos] = 0;
+     
+        //LEO confirmacion para seguir
+
+        cout << "terminar" << endl;
         close(socketComunicacion);
         sleep(1);
     }
@@ -161,25 +273,27 @@ void handlerSigInt(int sig ){
 
    
      close(socketComunicacion);
+        sem_close(semBlockServer);
+    sem_unlink("bloqueoServer");
      exit(sig);
 
 
 }
 
-int compararConPalabraOculta(string mensaje ,char * sendBuff ,int sizeBf,
-const char* pElegida,char* palabraOculta){
+list<char> compararConPalabraOculta(string mensaje ,char * sendBuff ,int sizeBf,
+const char* pElegida,char* palabraOculta,list<char> letras){
 
         bool coincidenciaPalabra = false;
     bool coincidenciaLetra = false;
 
-        
-        cout<< mensaje << endl;
+     
+    
         if(mensaje.size() == 1 ){
             const char  * l = mensaje.c_str();
 
-
+            letras.push_back(*l);
             //letra
-           cout << "letra : " << *l  << endl;
+       
                 while(*pElegida != '\0'){
                     if( *pElegida == *l  ){
                         coincidenciaLetra = true;
@@ -192,14 +306,16 @@ const char* pElegida,char* palabraOculta){
 
             if(coincidenciaLetra==true )
            {
+              
               cout<<"Letra correcta" << endl;
-               snprintf(sendBuff,sizeBf,"%s", "letra correcta\n");
+               snprintf(sendBuff,sizeBf,"%s\r\n", "letra correcta");
          
             }else{
+                finJuego++;
                 cout<<"Letra incorrecta" << endl;
-               snprintf(sendBuff,sizeBf,"%s", "letra incorrecta\n");
+               snprintf(sendBuff,sizeBf,"%s\r\n", "letra incorrecta");
             }
-            return LETRA;
+         //   return LETRA;
              
         }
         else
@@ -212,20 +328,63 @@ const char* pElegida,char* palabraOculta){
                     }
 
             if(coincidenciaPalabra==true)
-            {
+            {   
+                finJuego = INTENTOS;
                  cout<<"palabra correcta" << endl;
-               snprintf(sendBuff,sizeBf,"%s", "palabra correcta\n");
+               snprintf(sendBuff,sizeBf,"%s\r\n", "palabra correcta");
                
             }else
             {
+                finJuego++;
                 cout<<"palabra incorrecta" << endl;
-               snprintf(sendBuff,sizeBf,"%s", "palabra incorrecta\n");
+               snprintf(sendBuff,sizeBf,"%s\r\n", "palabra incorrecta");
             }
-             return PALABRA;
+           //  return PALABRA;
             
         }
 
 
-    return 0;
+    //return 0;
+    return letras;
+
+}
+
+
+void help()
+{   
+    cout << "-------------------------------------------------------" << endl;
+    cout << "Descripción : El Script ejecuta servidor que estara esperando a que un " << endl;
+    cout << "jugador se conecte para iniciar a jugar"<< endl;
+    cout << "\t- Ayuda del Script   Ejercicio5Servidor ..." << endl;
+    cout << "\t- Nombre Script:     ./Ejercicio5Servidor" << endl;
+    cout << "\t- Ejemplo de uso:    ./Ejercicio5Servidor 5000 (nro de puerto mayor a 1023)" << endl;
+    cout << "\t- Ejemplo de ayuda:    ./Ejercicio5Servidor -h"<< endl;
+    cout << "\t- Ejemplo de ayuda:    ./Ejercicio5Servidor --help" << endl;
+    cout << "\t- Fin de la ayuda... " << endl;
+    cout << "-------------------------------------------------------" << endl;
+ 
+}
+
+void recorrerLisra(list<char> lista){
+    
+    list<char> :: iterator pos;
+    pos = lista.begin();
+      cout << "Las letras ingresadas son : ";
+    while(pos!= lista.end()){
+        cout<< *pos << " ";
+        pos++;
+    }   
+    cout << endl;
+}
+
+ string listToString(list<char> lista){
+    string historial="";
+    list<char> :: iterator pos;
+    pos = lista.begin();
+    while(pos!= lista.end()){
+        historial+= *pos;
+        pos++;
+    }   
+return historial;
 
 }
